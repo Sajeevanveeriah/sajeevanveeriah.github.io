6c6f52d9a3de522b51539616c672513105af2090
Sajeevan Veeriah <sajeevanveeriah@gmail.com>
2026-09-02 16:49:09 +1000

    fix: lint Electron preload entry point

--- desktop/preload.cjs
+++ desktop/preload.cjs
@@ -1,4 +1,6 @@
 'use strict';
+/* CommonJS is required by Electron preload entry points. */
+/* eslint-disable @typescript-eslint/no-require-imports */
 
 const { contextBridge } = require('electron');
 const catalog = require('./product-catalog.json');
